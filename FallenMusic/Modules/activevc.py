e011d14fe95db8bc8b0bd30d08c8a704831dc955
yuukicans <calvinmanagement15@gmail.com>
2025-03-07 20:14:32 +0700

    yuuki activevc.py

--- FallenMusic/Modules/activevc.py
+++ FallenMusic/Modules/activevc.py
@@ -30,7 +30,7 @@ from FallenMusic.Helpers.inline import close_key
 
 @app.on_message(filters.command("activevc") & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("» ᴍᴇɴɢᴀᴍʙɪʟ ᴅᴀᴛᴀ ʏᴀɴɢ ᴍᴇᴍᴀᴋᴀɪ ʙᴏᴛ ᴀɴᴅᴀ...")
     chats = await get_active_chats()
     text = ""
     j = 0
@@ -46,10 +46,10 @@ async def activevc(_, message: Message):
             text += f"<b>{j + 1}. {title}</b> [`{chat}`]\n"
         j += 1
     if not text:
-        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄʙᴏᴛ...")
+        await mystic.edit_text("ᴛɪᴅᴀᴋ ᴀᴅᴀ ʏᴀɴɢ ᴍᴇᴍᴀᴋᴀɪ ʙᴏᴛ ᴀɴᴅᴀ...")
     else:
         await mystic.edit_text(
-            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :**\n\n{text}",
+            f"**ᴅᴀғᴛᴀʀ ɢʀᴏᴜᴘ ʏᴀɴɢ sᴇᴅᴀɴɢ ᴍᴇᴍᴀᴋᴀɪ ʙᴏᴛ ᴀɴᴅᴀ :**\n\n{text}",
             reply_markup=close_key,
             disable_web_page_preview=True,
         )
